f848f8fb00f5c5dda756ec01d5a4420ce83e7d8f
Tiffin Wale <tiffinwaleofficial@gmail.com>
2025-10-22 14:59:37 +0530

    gitignores and jest fix

--- services/monolith_backend/tests/simple.test.js
+++
@@ -1,9 +0,0 @@
-describe('Simple test', () => {
-  it('should pass', () => {
-    expect(1 + 1).toBe(2);
-  });
-  
-  it('should check that true is truthy', () => {
-    expect(true).toBeTruthy();
-  });
-}); 
